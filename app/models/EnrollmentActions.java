a797d21227b015212262fb65d8667cdb36fbd18d
Mitin Sharm <mitinsharma01@gmail.com>
2016-10-26 18:29:46 -0400

    pushed my files

--- app/models/EnrollmentActions.java
+++ app/models/EnrollmentActions.java
@@ -1,20 +1,25 @@
 package models;
 
 import com.fdflib.model.state.CommonState;
+import org.joda.time.DateTime;
+import play.data.format.Formats;
+
+import java.util.Date;
 
 /**
  * Created by Mitin on 10/26/2016.
  */
 public class EnrollmentActions extends CommonState {
 
-    public String date, time;
+    public Date date;
+    public Formats.DateTime time;
 
     public EnrollmentActions()
     {
         super();
     }
 
-    public EnrollmentActions(String d, String t)
+    public EnrollmentActions(Date d, Formats.DateTime t)
     {
         date=d;
         time=t;
