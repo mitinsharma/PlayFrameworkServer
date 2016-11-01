bacaa14d427c2c16ad13e22854c044a04624a2dd
Dominic <domrossillo@optonline.net>
2016-10-31 21:31:33 -0400

    added get terms with and with out history and delete and undelete term

--- app/services/TermService.java
+++ app/services/TermService.java
@@ -21,8 +21,33 @@ public class TermService extends FdfCommonServices {
         save(Term.class, term);
         return term;
     }
-
+    //get term by name
     public List<FdfEntity<Term>> getTermByName(String name) {
         return getEntitiesByValueForPassedField(Term.class, "name", name);
     }
+    //get current term
+    public Term getAllSections(){
+        return this.getAllCurrent(Term.class).get(0);
+
+    }
+
+    //get all terms with history
+    public List<FdfEntity<Term>> getAllTermsWithHistory() {
+        return this.getAll(Term.class);
+    }
+
+
+
+
+    //delete term
+    public void deleteTerm(Long id){
+        setDeleteFlag(Term.class,id,-1,-1);
+
+    }
+    //undelete section
+    public void undeleteTerm(Long id){
+        removeDeleteFlag(Term.class,id,-1,-1);
+    }
+
+
 }
